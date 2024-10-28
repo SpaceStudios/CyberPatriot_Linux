ff3283104d0336ee767131dd2e27c3157b9ed0ff
SpaceStudios <109873883+SpaceStudios@users.noreply.github.com>
2024-10-28 17:58:23 +0000

    Gained More Points

--- Ubuntu.sh
+++ Ubuntu.sh
@@ -1,11 +1,39 @@
 #!/bin/bash
 
-sudo apt-get update -y
-sudo apt-get upgrade -y
+#sudo apt-get update -y
+#sudo apt-get upgrade -y
+#sudo apt-get dist-upgrade
 
 users=()
 adminstrators=()
+fileName="Test.sh"
 
 echo "How many users are there on the system?"
 read userCount
-echo $userCount
+userCountint=$(($userCount))
+echo There are $userCountint users on this system.
+
+for i in $(seq $userCountint); do
+    echo What is the username of user $i?
+    read Username
+    echo What is the rank of $Username?
+    read Rank
+    users+=($Username)
+    if [ $Rank == 'Administrator' ] | [ $Rank == 'Admin' ]; then
+        adminstrators+=($Username)
+    fi 
+done
+
+echo Users are: ${users[*]}
+echo Admins are: ${adminstrators[*]}
+for user in $(cut -d: -f1,3 /etc/passwd | egrep ':[0-9]{4}$' | cut -d: -f1); do
+    echo $user
+    #sudo passwd $user
+    #Password should always be set to S3CUR3D_4O4
+done
+
+#sudo passwd -l root
+#sudo ufw enable
+#sudo ufw status
+
+sudo rm -r $fileName
